6a4dc18bcd6ddacf62a2a10ba67c4da9ba88a1fb
Abdelrahman Hesham <61296209+Red-Cloud2000@users.noreply.github.com>
2022-05-03 10:55:29 +0200

    Add files via upload

---
+++ headers.h
@@ -0,0 +1,10 @@
+unsigned char count = 0;
+unsigned char SEC1 = 0;
+unsigned char SEC2 = 0;
+unsigned char MIN1 = 0;
+unsigned char MIN2 = 0;
+unsigned char HOUR1 = 0;
+unsigned char HOUR2 = 0;
+
+void SegmentBySegment(unsigned char SMH , unsigned char spot);
+void Timer1_init_compare_mode(void);
